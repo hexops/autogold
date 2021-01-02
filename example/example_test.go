e670e7e21824e3a31cd6f886c83d5592b8c9870d
Stephen Gutekanst <stephen@hexops.com>
2021-01-02 16:20:13 -0700

    example: add inline example
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- example/example_test.go
+++ example/example_test.go
@@ -11,6 +11,14 @@ func TestEqual(t *testing.T) {
 	autogold.Equal(t, got)
 }
 
+func TestInline(t *testing.T) {
+	got := Bar()
+	// First parameter to Want can be omitted since we're only using a single Want in this test.
+	//
+	// 2nd parameter to Want is the wanted value - autogold will update this for us.
+	autogold.Want("", &Baz{Name: "Jane", Age: 31}).Equal(t, got)
+}
+
 func TestSubtest(t *testing.T) {
 	// Create one of these per sub-test value you want to compare.
 	want := autogold.Want("mysubtest", &Baz{Name: "Jane", Age: 31})
