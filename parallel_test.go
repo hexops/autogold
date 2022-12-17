7abe7d909e214663761a39f93728af94264a77b4
David Ackroyd <dackroyd@nine.com.au>
2022-12-17 17:36:45 +1100

    Fix data race when -update flag is enabled
    Usage of the 'cleaned' map wasn't correctly synchronized, so reads and
    writes by parallel tests can trigger a data race. By synchronizing
    around access to the map using a lock this behaviour can be corrected

---
+++ parallel_test.go
@@ -0,0 +1,34 @@
+package autogold_test
+
+import (
+	"testing"
+
+	"github.com/hexops/autogold"
+)
+
+func TestWant_parallel1(t *testing.T) {
+	testParallel(t, "1")
+}
+
+func TestWant_parallel2(t *testing.T) {
+	testParallel(t, "2")
+}
+
+func testParallel(t *testing.T, prefix string) {
+	t.Parallel()
+
+	testTable := []string{
+		prefix + "-first",
+		prefix + "-second",
+	}
+
+	for _, name := range testTable {
+		name := name
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			autogold.Equal(t, name)
+		})
+	}
+}
