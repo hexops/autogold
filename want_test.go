e31de3d02afd8bf0908817da94f60d4e7e970d61
David Ackroyd <dackroyd@nine.com.au>
2022-12-21 06:35:09 +1100

    Fix -update with Colliding Subtest Names (#31)
    Golden files are moved to a temp dir when -update is enabled, which
    allows clean-up of golden files which are not in use. The destination
    path in the temp dir did not consider the adjusted output directory
    for subtests, which meant that subtests with the same name would be
    copied to the same path in the temp dir, overwriting each other.
    This conflict leads to a number of issues, which results in tests
    failing despite producing the correct output.
    
    By adjusting the destination path inside the temp dir to match the
    relative dir used for the source (of the golden files), no conflict
    occurs when moving them to/from the temp dir.

--- want_test.go
+++ want_test.go
@@ -108,6 +108,36 @@ func Test_getPackageNameAndPath_subdir(t *testing.T) {
 	}
 }
 
+func TestEqual_subtestSameNames1(t *testing.T) {
+	testEqualSubtestSameNames(t)
+}
+
+func TestEqual_subtestSameNames2(t *testing.T) {
+	testEqualSubtestSameNames(t)
+}
+
+func testEqualSubtestSameNames(t *testing.T) {
+	t.Parallel()
+
+	parent := t.Name()
+
+	testTable := []string{
+		"first",
+		"second",
+		"third",
+	}
+
+	for _, name := range testTable {
+		name := name
+
+		t.Run(name, func(t *testing.T) {
+			// Subtests are intentionally not run in parallel, as that makes this issue more easily reproducible
+
+			Equal(t, fmt.Sprintf("%s :: %s", parent, name))
+		})
+	}
+}
+
 func Benchmark_getPackageNameAndPath_cached(b *testing.B) {
 	// Wipe the cache, as it was populated by other tests.
 	getPackageNameAndPathCacheMu.Lock()
